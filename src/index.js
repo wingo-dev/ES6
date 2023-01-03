cbb9f4515d223c7a337de92702265825aad60833
= <rukshanlancer@gmail.com>
2023-01-04 01:26:21 +0800

    es6 functions

--- src/index.js
+++ src/index.js
@@ -15,3 +15,28 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+var numbers = [3, 56, 2, 48, 5];
+
+const newNumbers = numbers.map((x) => {
+  return x * x;
+})
+console.log(newNumbers);
+
+const newNums = numbers.filter((x) => {
+  return x > 10;
+})
+
+console.log(newNums);
+
+const newfind = numbers.find((x) => {
+  return x > 10;
+})
+
+console.log(newfind);
+
+const reduceNum = numbers.reduce((acc, current) => {
+  return acc += current;
+})
+
+console.log(reduceNum);
